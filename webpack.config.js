00ad1b6e6c3a357e0cd441d6d72879ae39bc952f
Sharique Alam <56071706+zews78@users.noreply.github.com>
2020-04-11 21:33:51 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: "./script.js",
+  output: {
+    filename: "script.bundle.js",
+    path: __dirname
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  mode: "production"
+};
